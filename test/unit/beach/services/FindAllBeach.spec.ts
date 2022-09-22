0d0d8e8e4a490969ae21bdc451154a5d910214e6
Uriel Mori <moriuriel@gmail.com>
2022-09-22 12:04:20 -0300

    test: criando teste unitario para consulta

---
+++ test/unit/beach/services/FindAllBeach.spec.ts
@@ -0,0 +1,50 @@
+import { Beach } from '@/modules/beach/domain/entities/Beach';
+import { IBeachRepository } from '@/modules/beach/domain/repositories/IBeachRepository';
+import { BeachDataBaseRepository } from '@/modules/beach/infra/repositories/BeachDatabase.repository';
+import { BeachMemoryRepository } from '@/modules/beach/infra/repositories/BeachMemory.repository';
+import { FindAllBeachService } from '@/modules/beach/services';
+import { HttpStatus } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+
+const makeSut = async () => {
+  const moduleRef = await Test.createTestingModule({
+    providers: [
+      {
+        provide: BeachDataBaseRepository,
+        useFactory: () => {
+          return new BeachMemoryRepository();
+        },
+      },
+      FindAllBeachService,
+    ],
+  }).compile();
+
+  return {
+    findAllBeachService:
+      moduleRef.get<FindAllBeachService>(FindAllBeachService),
+    beachRepository: moduleRef.get<IBeachRepository>(BeachDataBaseRepository),
+  };
+};
+
+describe('Find All Beach Service', () => {
+  test('should be defined', async () => {
+    const { beachRepository, findAllBeachService } = await makeSut();
+
+    expect(beachRepository).toBeDefined();
+    expect(findAllBeachService).toBeDefined();
+  });
+
+  test('should be return beach list', async () => {
+    const { beachRepository, findAllBeachService } = await makeSut();
+
+    const beachEntity = new Beach('valid-beach', 'N', -28.3513, -48.8167);
+
+    await beachRepository.create(beachEntity);
+
+    const beachs = await findAllBeachService.execute();
+
+    expect(beachs).toHaveProperty('data');
+
+    expect(beachs.statusCode).toBe(HttpStatus.OK);
+  });
+});
